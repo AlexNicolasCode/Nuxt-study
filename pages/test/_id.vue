295c4d60c60f7d282d4e77f469a3e90432f404c9
Alex Nicolas <alexoliveira604@gmail.com>
2021-12-27 11:14:49 -0300

    feat: user must able to access test routes

---
+++ pages/test/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Continent: {{ continent }}</h1>
+    <h2>Mountain: {{ title }}</h2>
+    <p>Path: {{ $route.path }}</p>
+    <NuxtLink to="/">Back to Mountains</NuxtLink>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ params, error, payload }) {
+    if (payload) return { title: payload.title }
+  }
+}
+</script>
